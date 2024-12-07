57de5458250f288303c762865eeab242f00ea82c
Karthi Kuppan <karthi@jyoana.com>
2024-12-07 10:28:52 -0500

    Address the changes to volumes and added container names

--- scripts/rag.bat
+++ scripts/rag.bat
@@ -7,23 +7,22 @@ set "ScriptDir=%~dp0"
 REM Set the base directory for data and logs
 set "BASE_DIR=%ScriptDir%.."
 
-set directory[0]="cassandra2\data"
-set directory[1]="langflow\postgres"
-set directory[2]="langflow\data"
-set directory[3]="langflow\config"
-set directory[4]="rag\data"
-
-REM Create all data directories if it doesn't exist
-for /L %%i in (0,1,%count%) do (
-    mkdir /p %BASE_DIR%\data\!element[%%i]!
-)
-
 REM Check if the first argument is 'start' or 'stop'
 if "%1"=="start" (
-    echo Starting Docker Compose...
+
+    set directory[0]="cassandra2"
+    set directory[1]="langflow\postgres"
+    set directory[3]="langflow\config"
+
+    REM Create all data directories if it doesn't exist
+    for /L %%i in (0,1,%count%) do (
+        mkdir /p %BASE_DIR%\data\!element[%%i]!
+    )
+
+    echo Starting Docker containers...
     docker-compose up -d
 ) else if "%1"=="stop" (
-    echo Stopping Docker Compose...
+    echo Stopping Docker containers...
     docker-compose down
 ) else (
     echo Usage: %0 start|stop
